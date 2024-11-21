cf23faa54f24f41f052de85ebe9c95354ae89ae7
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-11-21 13:55:17 +0000

    Basic tool setup

---
+++ balance_subscriber/callbacks/__init__.py
@@ -0,0 +1,2 @@
+from .on_message import on_message
+from .on_connect import on_connect
---
+++ balance_subscriber/callbacks/on_message.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import uuid
+from pathlib import Path
+
+import paho.mqtt
+
+logger = logging.getLogger(__name__)
+
+
+def on_message(client: paho.mqtt.client.Client, userdata, msg: paho.mqtt.client.MQTTMessage):
+    """
+    The callback for when a PUBLISH message is received from the server.
+
+    https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.Client.on_message
+    """
+    timestamp = datetime.datetime.fromtimestamp(msg.timestamp, datetime.timezone.utc)
+    logger.debug(":%s:%s:%s", timestamp.isoformat(), msg.topic, msg.payload)
+
+    # Serialise message
+    filename = f"{uuid.uuid4().hex}.bin"
+    path = Path() / filename
+    with path.open('wb') as file:
+        file.write(msg.payload)
+        logger.debug("Wrote %s", file.name)
