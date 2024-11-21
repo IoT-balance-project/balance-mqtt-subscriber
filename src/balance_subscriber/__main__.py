01662cb681524a7e40b392cf1fc5d1850dacf7df
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-11-21 16:05:35 +0000

    Implement systemd service

--- src/balance_subscriber/__main__.py
+++ src/balance_subscriber/__main__.py
@@ -1,6 +1,9 @@
 import argparse
 import importlib.metadata
 import logging
+import os
+from pathlib import Path
+
 import paho.mqtt.client
 
 import balance_subscriber.callbacks
@@ -8,7 +11,7 @@ import balance_subscriber.callbacks
 logger = logging.getLogger(__name__)
 
 DESCRIPTION = """
-This is a simple script to test that the MQTT broker is working as expected. Use topics "#" to listen to all channels.
+This is an MQTT subscriber that serialises incoming messages.
 """
 
 
@@ -17,13 +20,37 @@ def get_args():
     Command-line arguments
     """
     parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument(
+        "--data_dir",
+        type=Path,
+        help="Directory to save messages to.",
+        default=os.getenv("DATA_DIR"),
+    )
     parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument(
-        "topics", nargs="*", help="Topics to subscribe to, default: all", default="#"
+        "topics",
+        nargs="*",
+        help="Topics to subscribe to, default: all",
+        default=os.getenv("TOPICS", "#"),
     )
-    parser.add_argument("--host", type=str, default="localhost")
-    parser.add_argument("--port", type=int, default=1883)
-    parser.add_argument("--keepalive", type=int, default=60)
+    parser.add_argument(
+        "--host",
+        type=str,
+        default=os.getenv("HOST", "localhost"),
+        help="MQTT broker host",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=int(os.getenv("PORT", 1883)),
+        help="MQTT broker port",
+    )
+    parser.add_argument(
+        "--keepalive",
+        type=int,
+        default=int(os.getenv("KEEP_ALIVE", 60)),
+        help="MQTT broker keep-alive interval",
+    )
     parser.add_argument(
         "--version",
         action="version",
@@ -32,13 +59,16 @@ def get_args():
     return parser.parse_args()
 
 
-def get_client(topics) -> paho.mqtt.client.Client:
+def get_client(topics: set[str], data_dir: Path) -> paho.mqtt.client.Client:
+    if not data_dir:
+        raise ValueError("No data directory specified")
+
     # Initialise client
     client = paho.mqtt.client.Client(paho.mqtt.client.CallbackAPIVersion.VERSION2)
     # https://eclipse.dev/paho/files/paho.mqtt.python/html/index.html#logger
     client.enable_logger()
     # Make the topics available to the on_connect callback
-    client.user_data_set(dict(topics=topics))
+    client.user_data_set(dict(topics=topics, data_dir=data_dir))
 
     # Register callbacks
     client.on_connect = balance_subscriber.callbacks.on_connect
@@ -52,7 +82,7 @@ def main():
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
     # Connect to message broker
-    client = get_client(topics=args.topics)
+    client = get_client(topics=args.topics, data_dir=args.data_dir)
     client.connect(host=args.host, port=args.port, keepalive=args.keepalive)
 
     # Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
--- src/balance_subscriber/callbacks/on_message.py
+++ src/balance_subscriber/callbacks/on_message.py
@@ -3,25 +3,30 @@ import logging
 import uuid
 from pathlib import Path
 
-import paho.mqtt
+import paho.mqtt.client
 
 logger = logging.getLogger(__name__)
 
 
 def on_message(
-    client: paho.mqtt.client.Client, userdata, msg: paho.mqtt.client.MQTTMessage
+    client: paho.mqtt.client.Client, userdata: dict, msg: paho.mqtt.client.MQTTMessage
 ):
     """
     The callback for when a PUBLISH message is received from the server.
 
+    on_message callback
     https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.Client.on_message
+
+    MQTT message class
+    https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client.MQTTMessage
     """
     timestamp = datetime.datetime.fromtimestamp(msg.timestamp, datetime.timezone.utc)
     logger.debug(":%s:%s:%s", timestamp.isoformat(), msg.topic, msg.payload)
 
     # Serialise message
-    filename = f"{uuid.uuid4().hex}.bin"
-    path = Path() / filename
+    filename = f"{msg.mid}.bin"  # message identifier
+    # Create a directory based on topic name
+    path = Path(userdata["data_dir"]) / msg.topic / filename
     with path.open("wb") as file:
         file.write(msg.payload)
         logger.debug("Wrote %s", file.name)
