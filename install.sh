f0bb1cf71d429d0327288f946d7d42028f59653e
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2024-11-22 15:49:58 +0000

    Add Python package to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+venv_dir="/opt/balance-subscriber/venv"
+
+# Install requirements
+apt install -qqq python3.12
+
+# Create virtual environment
+mkdir --parents "$(dirname $venv_dir)"
+python3 -m venv $venv_dir
+# Update pip
+$venv_dir/bin/python -m pip install -q --upgrade pip
+$venv_dir/bin/pip install --upgrade balance-subscriber
+
 # Install the systemd service
 cp --verbose ./systemd/balance-subscriber.service /etc/systemd/system/balance-subscriber.service
