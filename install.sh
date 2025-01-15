a63c73fc8a663090384cac0999ba1dbee36bb7f4
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-01-15 11:30:52 +0000

    Add option to specify package version

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 set -e
 
 # Install the MQTT subscriber service
+# Usage:
+# sudo bash -x install.sh $version
+# version: The version of the package to install
 
 # Set options
 venv_dir="/opt/balance-subscriber/venv"
 python="$venv_dir/bin/python"
 pip="$python -m pip"
+version="${1:-*}"
 
 # Install requirements
 apt install -qqq python3.12 python3.12-venv
@@ -17,7 +21,7 @@ python3 -m venv $venv_dir
 # Update pip
 $python -m pip install -q --upgrade pip
 # Install the subscriber package
-$pip install --upgrade balance-subscriber
+$pip install --upgrade balance-subscriber="$version"
 
 # Install the systemd service
 cp --verbose ./systemd/balance-subscriber.service /etc/systemd/system/balance-subscriber.service
